e2badb73f0c8eefa26ac3ca67591bc5be2c52fb1
Styve7 <youmbisteve42@gmail.com>
2024-08-09 16:25:11 +0100

    implement json object

---
+++ js/data.js
@@ -0,0 +1,93 @@
+const data = [
+    {
+        id: 1,
+        description: 'chambre moderne nouvellement construite a pk15 a louer TRÈS proche de là route 2 chambres, salon, cuisine, 3 douches avec parking.',
+        price: 35000,
+        location: "pk15",
+        images:1,
+        type:"chambre",
+    },
+    {
+        id: 2,
+        description: 'STUDIO ENTIÈREMENT CLIMATISÉ À LOUER À DOUALA PK12 DANS UNE SOMPTUEUSE RÉSIDENCE SÉCURISÉE.IL COMPREND 01 salon01 cuisine 01 douche ( dans la chambre)01 chambre Eau forage Lumière prépayée Parking fermé ',
+        price: 45000,
+        location: "Pk12",
+        images:2,
+        type:"studio",
+
+    },
+    
+    {
+        id: 3,
+        description: ' Villa à louer à Logbesou, à 400 mètres du goudron, en bordure de rue pavée. Avec parking et forage 01 Vaste salon 01 grande chambre avec grands placards coulissants01 grande douche bien équipéecuisine équipée avec coffre de rangementCaméras de surveillance 🎥🎥🎥🎥Installation électrique de la climatisation au Salon et dans la chambreParking. ',
+        price: 200000,
+        location: "Logbesou",
+        images:3,
+        type:"Villa",
+
+    },
+    {
+        id: 4,
+        description:'Jolie villa à louer en plein cœur de Deido au niveau de l école publique Composition :1 chambre 1 salon  1 douche interne moderne  1cuisine moderne  1 véranda  Une cours en pavé',
+        price: 250000,
+        location: "Deido",
+        images:4,
+        type:"Villa",
+
+    },
+    {
+        id: 5,
+        description: 'Appartement nouvellement construit et haut standing à kotto avec caractéristiques comme suit Caractéristiques.02 grand studio moderne en location (50.000f)01 chambre moderne en location (30.000f)01 Villa de 04 chambres 01 salon 01 douche très bien placé accessible à tout type de véhicule quartier résidentiel et sécurisé.; ',
+        price:160000 ,
+        location: "Kotto",
+        images:5,
+        type:"Appartement",
+
+    },
+    {
+        id: 6,
+        description: ' Vaste et Magnifique chambres moderne staffée ont vient a peine de libérée ( à logbessou à zéro pas du goudron )  ☝🏽2ème Étage 1 Balcons Parking interne ',
+        price:50000 ,
+        location: "Logbessou",
+        images:6,
+        type:"Chambre",
+
+    },
+    {
+        id: 7,
+        description: 'Studio moderne à louer a  logbessou, dans un etage dans la barrière avec parking a pied de la route( 300m)38.78m² de surface2 pièces1 chambre1 salle de bain  ',
+        price:45000 ,
+        location: "Logbessou",
+        images:7,
+        type:"studio",
+
+    },
+    {
+        id: 8,
+        description: 'Studio moderne chambre salon cuisine douche balcon personnel parking forage et compteur prépayé 3 chambres 2 douches salon cuisine parking  ',
+        price:50000 ,
+        location: "Yassa",
+        images:8,
+        type:"studio",
+
+    },
+    {
+        id: 9,
+        description: 'Studio meublé Douala Bonamoussadi Denver2 chambres salon 2 douches cuisine ',
+        price:60000 ,
+        location: "Bonamoussadi",
+        images:9,
+        type:"studio",
+
+    },
+    {
+        id: 10,
+        description: '  Studio Moderne Staffé, disponible à Deïdo ( 50m de la route ) { Travaux à refaire}  1 chambre avec placard 1 salon 1 douche moderne 1 cuisine   2 balcons  ',
+        price: 55000,
+        location: "Deido",
+        images:10,
+        type:"studio",
+
+    },
+    
+]
